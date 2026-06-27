fce7d364dec59f4fdb1314e84f094298ec52ad16
taz1003 <li9vyrit@students.codeinstitute.net>
2026-06-27 10:56:03 +0200

    Initial commit, add Emailjs in js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+emailjs.init("YOUR_PUBLIC_KEY");
+function sendMail(contactForm) {
+  emailjs
+    .send("service_xe3eg9i", "rosie", {
+      from_name: contactForm.name.value,
+      email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+    // return false; // To block from loading a new page
+}

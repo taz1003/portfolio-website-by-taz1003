5f7ed955223f87b76d9e69e5e4e1fe88d5b4bd61
taz1003 <li9vyrit@students.codeinstitute.net>
2026-06-28 15:18:23 +0200

    Add footer, back-to-top button, style and script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
 function sendMail(contactForm) {
-  emailjs
-    .send("service_xe3eg9i", "rosie", {
-      from_name: contactForm.name.value,
-      email: contactForm.emailaddress.value,
-      project_request: contactForm.projectsummary.value,
-    })
-    .then(
-      function (response) {
-        console.log("SUCCESS", response);
-      },
-      function (error) {
-        console.log("FAILED", error);
-      }
-    );
-    // return false; // To block from loading a new page
+	emailjs
+		.send("service_xe3eg9i", "rosie", {
+			from_name: contactForm.name.value,
+			email: contactForm.emailaddress.value,
+			project_request: contactForm.projectsummary.value,
+		})
+		.then(
+			function (response) {
+				console.log("SUCCESS", response);
+			},
+			function (error) {
+				console.log("FAILED", error);
+			},
+		);
+	// return false; // To block from loading a new page
 }
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+	if (window.scrollY > 400) {
+		topBtn.style.display = "block";
+	} else {
+		topBtn.style.display = "none";
+	}
+});
+
+topBtn.addEventListener("click", () => {
+	window.scrollTo({
+		top: 0,
+
+		behavior: "smooth",
+	});
+});
